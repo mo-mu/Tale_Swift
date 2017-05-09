b5c35f327e7fd7f034f682e95eeee8c866f730c6
songmho <songmho@gmail.com>
2017-05-09 22:49:18 +0900

    0. 프로젝트 생성 1. 스플래시이미지 구성 2. 메인페이지 구성 3. 글쓰기 페이지 구성

--- TailSwift/ViewController.swift
+++ TailSwift/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtChange: UILabel!
+    @IBOutlet weak var txtQst: UILabel!
+    @IBOutlet weak var btnWrite: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        self.navigationController!.navigationBar.isTranslucent = true
+        
+        let gesturetxtChange = UITapGestureRecognizer(target: self, action: #selector(self.changeQst(_:)))
+        
+        self.txtChange.addGestureRecognizer(gesturetxtChange)
+        
+        txtChange.isUserInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    func changeQst(_ sender: UITapGestureRecognizer) {
+        txtQst.text = "test"
+        print("hello")
+    }
+ 
 }
 
---
+++ TailSwift/WriteViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WriteViewController.swift
+//  TailSwift
+//
+//  Created by songmho on 2017. 5. 9..
+//  Copyright © 2017년 momu. All rights reserved.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backClicked(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
