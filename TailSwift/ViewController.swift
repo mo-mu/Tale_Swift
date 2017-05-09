6a68365859f7bd8f9864f30bcfc22df321460749
songmho <songmho@gmail.com>
2017-05-09 12:02:50 +0900

    Initial Commit

---
+++ TailSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TailSwift
+//
+//  Created by songmho on 2017. 5. 9..
+//  Copyright © 2017년 momu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
